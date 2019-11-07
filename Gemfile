f13bf5baae45807cb01fdbc6cd42fa02f9ac68e9
Evan Wichlarz <evanwichlarz1@hotmail.fr>
2019-11-07 17:00:22 +0100

    all cocktail application

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.3'
 
 gem 'autoprefixer-rails'
+gem "flutie"
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,62 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  # GET /cocktails
+  # GET /cocktails.json
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  # GET /cocktails/1
+  # GET /cocktails/1.json
+  def show
+    @dose = Dose.new
+  end
+
+  # GET /cocktails/new
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  # GET /cocktails/1/edit
+  def edit
+    @dose = Dose.new
+  end
+
+  # POST /cocktails
+  # POST /cocktails.json
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+  # PATCH/PUT /cocktails/1
+  # PATCH/PUT /cocktails/1.json
+  def update
+
+  end
+
+  # DELETE /cocktails/1
+  # DELETE /cocktails/1.json
+  def destroy
+    @cocktail.destroy
+    respond_to do |format|
+      format.html { redirect_to cocktails_url, notice: 'Cocktail was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
